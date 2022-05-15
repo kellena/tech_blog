ab9834c1feb0c61c087a89fb207e71dd03c0c95a
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-15 14:07:53 -0500

    built post route for login function in userroutes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,6 +11,36 @@ router.get('/login', async (req, res) => {
     }
 })
 
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+  
+    if (!user) {
+        return res.status(400).json({ message: "The username is incorrect." });
+    }
+
+    const validPassword = await user.checkPassword(req.body.password);
+  
+    if (!validPassword) {
+        return res.status(400).json({ message: "The password is incorrect." });
+    }
+  
+    req.session.save(() => {
+        req.session.user_id = user.id;
+        req.session.logged_in = true;
+        res.status(200).json({ user: user, message: "You are now logged in." });
+    });
+
+    } catch (err) {
+        res.json(err);
+    }
+
+});
+
 router.get('/register', async (req, res) => {
     try {
         res.render('register')
