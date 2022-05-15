57641d8d28d4f646009e37f9c65300239d25f1f2
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-15 15:29:30 -0500

    adjusted models spacing for debugging clarity

--- models/Post.js
+++ models/Post.js
@@ -3,44 +3,53 @@ const sequelize = require('../config/config');
 
 class Post extends Model {}
 
-  Post.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+Post.init(
+
+    {
+
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        postTitle: {
+            type: DataTypes.STRING(25),
+            allowNull: false,
+            unique: true,
+        },
+
+        postContent: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+
+        dateCreated: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },  
+
     },
-    postTitle: {
-      type: DataTypes.STRING(25),
-      allowNull: false,
-      unique: true,
-    },
-    postContent: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    dateCreated: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },  
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post',
-  }
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+
 );
 
 
